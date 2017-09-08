aeb290402f59f52d534889cb4ec16de610ceb981
Keiji Kimura <kgeewimp@gmai.com>
2017-09-08 19:35:26 +0200

    Add gen_txt.sh

---
+++ LPfile_MISOCP/result/gen_txt.sh
@@ -0,0 +1,28 @@
+#/bin/sh
+#chmod u+x gen.sh
+generate(){
+   file=$1
+   buf=${1##*/}
+   name=${buf%.*}
+   txtname=$2
+   echo "read $file"
+   echo "read $file" >> $txtname
+   echo "opt"
+   echo "opt" >> $txtname
+}
+
+time=10000
+txtname=run.txt
+
+rm -f $txtname
+echo "generate $txtname"
+echo "set timelimit $time"
+echo "set timelimit $time" >> $txtname
+
+for file in `ls ../output/*.lp`
+do
+   generate $file $txtname
+done
+
+echo "q"
+echo "q" >> $txtname
