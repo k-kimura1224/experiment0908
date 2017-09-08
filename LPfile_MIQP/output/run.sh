658281aa62a0670a29f683dbdb9d0ddd54877714
Keiji Kimura <kgeewimp@gmai.com>
2017-09-08 20:22:28 +0200

    Add LPfile_MIQP

---
+++ LPfile_MIQP/output/run.sh
@@ -0,0 +1,33 @@
+#/bin/sh
+#chmod u+x gen.sh
+generate(){
+   file=$1
+   buf=${1##*/}
+   name=${buf%.*}
+   k=$2
+   ../bin/genelpfile $file $k > ${name}_${k}.lp
+}
+
+k=30
+
+for file in `ls ../../data/*.linereg`
+do
+   generate $file $k
+   echo "generate lp-file from ${file}"
+done
+
+k=20
+
+for file in `ls ../../data/*.linereg`
+do
+   generate $file $k
+   echo "generate lp-file from ${file}"
+done
+
+k=10
+
+for file in `ls ../../data/*.linereg`
+do
+   generate $file $k
+   echo "generate lp-file from ${file}"
+done
